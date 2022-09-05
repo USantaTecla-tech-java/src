da5d4dd099b0ddc0789364db83d77470a047be05
Setillo <setillofm@gmail.com>
2022-09-05 13:59:59 +0200

    refactor

--- src/main/java/es/usantatecla/itinerario/App.java
+++ src/main/java/es/usantatecla/itinerario/App.java
@@ -1,9 +1,9 @@
-package es.usantatecla;
+package es.usantatecla.itinerario;
 
 public class App {
     
     public static void main(String[] args) {
-        System.out.println("hola");
+        System.out.println("adios");
     }
     
 }
---
+++ src/test/java/es/usantatecla/itinerario/AppTest.java
@@ -0,0 +1,13 @@
+package es.usantatecla.itinerario;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertFalse(false);
+    }
+
+}
