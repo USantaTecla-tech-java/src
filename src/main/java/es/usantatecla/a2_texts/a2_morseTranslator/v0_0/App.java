e3369954dfab65d13e30153838169dd0024210d9
Setillo <setillofm@gmail.com>
2022-09-29 07:30:55 +0200

    refactor

--- src/main/java/es/usantatecla/a2_texts/a2_morseTranslator/v0_0/App.java
+++ src/main/java/es/usantatecla/a2_texts/a2_morseTranslator/v0_0/App.java
@@ -1,25 +1,31 @@
-const { Console } = require("./console");
+package es.usantatecla.a2_texts.a2_morseTranslator.v0_0;
 
-const console = new Console();
-const LETTERS = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"
-    , "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const MORSE = ["/ ", ".- ", "-... ", "-.-. ", "-.. ", ". ", "..-. ", "--. ", ".... "
-    , ".. ", ".--- ", "-.- ", ".-.. ", "-- ", "-. ", "--- ", ".--. ", "--.- ", ".-. "
-    , "... ", "- ", "..- ", "...- ", ".-- ", "-..- ", "-.-- ", "--.. "];
-const sentence = console.readString("Dime algo: ");
-let message = "";
-for (let i = 0; i < sentence.length; i++) {
-    let char = sentence[i];
-    if ('A' <= char && char <= 'Z'){
-        char = toLowerCase(sentence[i]);
-    } else if (char < 'a' || 'z' < char) {
-        char = ' ';
-    }
-    for (let j = 0; j < MORSE.length; j++) {
-        if (char == LETTERS[j]) {
-            message += MORSE[j];
+import es.usantatecla.utils.Console;
+
+public class App {
+
+    public static void main(String[] args) {
+        Console console = new Console();
+        final char[] LETTERS = new char[] { ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        final String[] MORSE = new String[] { "/ ", ".- ", "-... ", "-.-. ", "-.. ", ". ", "..-. ", "--. ", ".... ",
+                ".. ", ".--- ", "-.- ", ".-.. ", "-- ", "-. ", "--- ", ".--. ", "--.- ", ".-. ", "... ", "- ", "..- ",
+                "...- ", ".-- ", "-..- ", "-.-- ", "--.. " };
+        final String sentence = console.readString("Dime algo: ");
+        String message = "";
+        for (int i = 0; i < sentence.length(); i++) {
+            int character = sentence.charAt(i);
+            if ('A' <= character && character <= 'Z') {
+                character = sentence.charAt(i) - 'A';
+            } else if (character < 'a' || 'z' < character) {
+                character = ' ';
+            }
+            for (int j = 0; j < MORSE.length; j++) {
+                if (character == LETTERS[j]) {
+                    message += MORSE[j];
+                }
+            }
         }
+        console.writeln("La frase " + sentence + "se traduce a " + message);
     }
 }
-console.writeln(`La frase "${sentence}"
-se traduce a "${message}"`);
--- src/main/java/es/usantatecla/a6_collections/a3_matrixTranspose/v0_0/App.java
+++ src/main/java/es/usantatecla/a6_collections/a3_matrixTranspose/v0_0/App.java
@@ -1,21 +1,39 @@
-const { Console } = require("./console");
+package es.usantatecla.a6_collections.a3_matrixTranspose.v0_0;
 
-let matrix = [
-  [1, 2, 3, 4, 5],
-  [6, 7, 8, 9, 10],
-  [11, 12, 13, 14, 15],
-  [16, 17, 18, 19, 20],
-  [21, 22, 23, 24, 25],
-];
+import es.usantatecla.utils.Console;
 
-let transposed = [];
-for (let i = 0; i < matrix.length; i++) {
-  transposed[i] = [];
-  for (let j = 0; j < matrix[0].length; j++) {
-    transposed[i][j] = matrix[j][i];
-  }
+public class App {
+
+  public static void main(String[] args) {
+    Console console = new Console();
+    int[][] matrix = new int[][] {
+        { 1, 2, 3, 4, 5 },
+        { 6, 7, 8, 9, 10 },
+        { 11, 12, 13, 14, 15 },
+        { 16, 17, 18, 19, 20 },
+        { 21, 22, 23, 24, 25 },
+    };
+
+    int[][] transposed = new int[matrix.length][];
+    for (int i = 0; i < matrix.length; i++) {
+      transposed[i] = new int[matrix[i].length];
+      for (int j = 0; j < matrix[i].length; j++) {
+        transposed[i][j] = matrix[j][i];
+      }
+    }
+    console.writeln("Matriz original: ");
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length; j++) {
+        console.write(matrix[j][i]+", ");
+      }
+      console.writeln();
+    }
+    console.writeln("Matriz original: ");
+    for (int i = 0; i < transposed.length; i++) {
+      for (int j = 0; j < transposed[i].length; j++) {
+        console.write(matrix[j][i]+", ");
+      }
+      console.writeln();
+    }
+    }
 }
-
-const console = new Console();
-console.writeln(`Matriz original: ${matrix}`);
-console.writeln(`Matriz original: ${transposed}`);
