b1398e8bc72046c2304fd441e047170c47c36bf3
usantatecla@gmail.com <usantatecla@gmail.com>
2023-03-23 17:22:34 +0100

    refactor

--- src/main/java/es/usantatecla/a5_units/a1_interval/a7_parametrized/App.java
+++ src/main/java/es/usantatecla/a5_units/a1_interval/a7_parametrized/App.java
@@ -4,23 +4,17 @@ class App {
 
     public static void main(String[] args) {
         final int SIZE = 3;
-        Interval<Integer>[] intervals = new Interval[SIZE];
-        for (int i = 0; i < intervals.length; i++) {
-            intervals[i] = new Interval<Integer>(-i - 1, i + 1);
+        Interval<Integer>[] intIntervals = new Interval[SIZE];
+        for (int i = 0; i < intIntervals.length; i++) {
+            intIntervals[i] = new Interval<Integer>(-i - 1, i + 1);
         }
-        App.scenarioPrametrized(intervals, SIZE);
+        App.scenarioPrametrized(intIntervals, SIZE);
 
-        Interval<Fraction>[] fractions = new Interval[SIZE];
-        for (int i = 0; i < fractions.length; i++) {
-            fractions[i] = new Interval<Fraction>(new Fraction(-i - 1, i + 1), new Fraction(i + 2, i + 1));
+        Interval<Fraction>[] fractionIntervals = new Interval[SIZE];
+        for (int i = 0; i < fractionIntervals.length; i++) {
+            fractionIntervals[i] = new Interval<Fraction>(new Fraction(-i - 1, i + 1), new Fraction(i + 2, i + 1));
         }
-        App.scenarioPrametrized(fractions, SIZE);
-
-        Interval<Integer>[] integers = new Interval[SIZE];
-        for (int i = 0; i < integers.length; i++) {
-            integers[i] = new Interval<Integer>(-i,i);
-        }
-        App.scenarioPrametrized(integers, SIZE);
+        App.scenarioPrametrized(fractionIntervals, SIZE);
     }
 
     private static <E> void scenarioPrametrized(Interval<? extends Comparable<E>>[] intervals, final int SIZE) {
