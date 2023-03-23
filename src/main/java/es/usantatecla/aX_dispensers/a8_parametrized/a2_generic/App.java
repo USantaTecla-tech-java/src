c8213a3ccd9b58dc548c3e38d6d22d7443ac154f
usantatecla@gmail.com <usantatecla@gmail.com>
2023-03-23 10:19:41 +0100

    refactor

--- src/main/java/es/usantatecla/aX_dispensers/a8_parametrized/a2_generic/App.java
+++ src/main/java/es/usantatecla/aX_dispensers/a8_parametrized/a2_generic/App.java
@@ -5,6 +5,7 @@ import es.usantatecla.aX_dispensers.a8_parametrized.a2_generic.utils.Fraction;
 import es.usantatecla.aX_dispensers.a8_parametrized.a2_generic.utils.Interval;
 import es.usantatecla.aX_dispensers.a8_parametrized.a2_generic.utils.Iterator;
 import es.usantatecla.aX_dispensers.a8_parametrized.a2_generic.dispensers.Dispenser;
+import es.usantatecla.aX_dispensers.a8_parametrized.a2_generic.dispensers.DispenserException;
 import es.usantatecla.aX_dispensers.a8_parametrized.a2_generic.dispensers.EmptyDispenserException;
 import es.usantatecla.aX_dispensers.a8_parametrized.a2_generic.dispensers.FullDispenserException;
 import es.usantatecla.aX_dispensers.a8_parametrized.a2_generic.dispensers.boundedDispensers.BoundedQueue;
@@ -27,7 +28,7 @@ public class App {
 
 		Fraction[] fraction10 = new Fraction[10];
 		for (int i = 0; i < 10; i++) {
-			fraction10[i] = new Fraction(-i, i+1);
+			fraction10[i] = new Fraction(-i, i + 1);
 		}
 		Fraction[] fraction5 = new Fraction[5];
 		for (int i = 0; i < 5; i++) {
@@ -35,7 +36,19 @@ public class App {
 		}
 		App.exampleParametrized(fraction10, fraction5);
 		App.exampleParametrized(
-			new Integer[]{0,1,2,3,4,5,6,7,8,9}, new Integer[]{-2,-1,0,1,2});
+				new Integer[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new Integer[] { -2, -1, 0, 1, 2 });
+
+		App.sumFractions(App.getFractionBoundedQueue());
+		App.sumInts(App.getIntBoundedQueue());
+		try {
+			BoundedQueue<Integer> boundedQueue = new BoundedQueue<Integer>(1);
+			boundedQueue.add(2);
+			// boundedQueue.add(new Fraction(1, 2)); No compila!!!
+			int element = (int) boundedQueue.remove();
+			Console.getInstance().writeln(element);
+		} catch (DispenserException e) {
+			e.printStackTrace();
+		}
 	}
 
 	private static <E> void exampleParametrized(E[] in10, E[] in5) {
@@ -51,10 +64,10 @@ public class App {
 
 		Console.getInstance().writeln("------- Metemos 10");
 		try {
-			for(E element : in10){
+			for (E element : in10) {
 				dispenser.add(element);
 			}
-		} catch (FullDispenserException ex){
+		} catch (FullDispenserException ex) {
 			Console.getInstance().writeln("Dispensador lleno, seguimos");
 		}
 		App.writelnDispenser(dispenser.getIterator());
@@ -64,17 +77,17 @@ public class App {
 			for (int i = 0; i < 5; i++) {
 				dispenser.remove();
 			}
-		} catch (EmptyDispenserException ex){
+		} catch (EmptyDispenserException ex) {
 			Console.getInstance().writeln("!!! Dispensador vacío, seguimos");
 		}
 		App.writelnDispenser(dispenser.getIterator());
 
 		Console.getInstance().writeln("------- Metemos 5");
 		try {
-			for(E element : in5){
+			for (E element : in5) {
 				dispenser.add(element);
 			}
-		} catch (FullDispenserException ex){
+		} catch (FullDispenserException ex) {
 			Console.getInstance().writeln("!!! Dispensador lleno, seguimos");
 		}
 		App.writelnDispenser(dispenser.getIterator());
@@ -86,6 +99,57 @@ public class App {
 			Console.getInstance().writeln(position + ": " + iterator.next());
 			position++;
 		}
+	}
+
+	private static BoundedQueue<Fraction> getFractionBoundedQueue() {
+		BoundedQueue<Fraction> boundedQueue = new BoundedQueue<Fraction>(5);
+		try {
+			boundedQueue.add(new Fraction(1, 2));
+			boundedQueue.add(new Fraction(1, 2));
+		} catch (FullDispenserException ex) {
+			ex.printStackTrace();
+		}
+		return boundedQueue;
+	}
+
+	private static Fraction sumFractions(BoundedQueue<Fraction> boundedQueue) {
+		assert boundedQueue != null;
+
+		Fraction acum = new Fraction(0);
+		try {
+			while (boundedQueue.isEmpty()) {
+				acum = acum.add(boundedQueue.remove());
+			}
+		} catch (EmptyDispenserException ex) {
+			ex.printStackTrace();
+		}
+		return acum;
+	}
+
+	private static BoundedQueue<Integer> getIntBoundedQueue() {
+		try {
+			BoundedQueue<Integer> boundedQueue = new BoundedQueue<Integer>(5);
+			boundedQueue.add(1);
+			boundedQueue.add(1);
+			return boundedQueue;
+		} catch (FullDispenserException ex) {
+			ex.printStackTrace();
+		}
+		return null;
+	}
+
+	private static int sumInts(BoundedQueue<Integer> boundedQueue) {
+		assert boundedQueue != null;
+
+		int acum = 0;
+		try {
+			while (boundedQueue.isEmpty()) {
+				acum += boundedQueue.remove();
+			}
+		} catch (EmptyDispenserException ex) {
+			ex.printStackTrace();
+		}
+		return acum;
 	}
 
 }
