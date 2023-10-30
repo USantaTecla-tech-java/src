a774cb63a6d84cec3fd7e0a592fb70c7caaf426e
usantatecla@gmail.com <usantatecla@gmail.com>
2023-10-31 00:08:42 +0100

    refactor

--- src/main/java/es/usantatecla/aX_menu/a4_extends/a1_modelMenu/Option.java
+++ src/main/java/es/usantatecla/aX_menu/a4_extends/a1_modelMenu/Option.java
@@ -4,18 +4,14 @@ abstract class Option {
 
     private String title;
 
-    public Option(String title) {
+    protected Option(String title) {
         this.title = title;
     }
 
     public abstract void interact();
 
     public void showTitle(int index) {
-        Console.getInstance().writeln(index + ". " + this.getTitle());
-    }
-
-    protected String getTitle() {
-        return this.title;
+        Console.getInstance().writeln(index + ". " + this.title);
     }
 
 }
