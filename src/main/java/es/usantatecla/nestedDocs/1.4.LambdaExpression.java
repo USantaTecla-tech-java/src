5779b7c75ffd66744b10becac2422fc8be62762f
usantatecla@gmail.com <usantatecla@gmail.com>
2024-12-16 14:49:15 +0100

    refactor

--- src/main/java/es/usantatecla/nestedDocs/1.4.LambdaExpression.java
+++ src/main/java/es/usantatecla/nestedDocs/1.4.LambdaExpression.java
@@ -3,22 +3,16 @@ package es.usantatecla.nestedDocs;
 // 1.4. Expresiones Lambda
 // funciones anónimas, objetos de clases anónimas con un único método
 
-interface IntervalConsumer<E extends Comparable<E>> {
-  void accept(Interval<E> interval);
-}
-
-class X {
-  public static void main(String arg[]){
-    final double SHIFTMENT = 1;
-    IntervalConsumer<Integer> function = (Interval<Integer> interval) -> Console.getInstance().writeln(interval.toString());
-  
-  }
-}
-
-// es/usantatecla/aX_menu/a8_anonymus anonimas en opciones pero no en main
-// es/usantatecla/aX_menu/a9_lambda funciones en opciones! No en main!!!
+// bien
+// es/usantatecla/aX_report/a1_extends
+// es/usantatecla/aX_report/a2_anonymus
+// es/usantatecla/aX_report/a3_functioinal
 
 // Ir a 1.4.1. FunctionalInterface
+
+// bien!?!?
+// es/usantatecla/aX_menu/a8_anonymus anonimas en opciones pero no en main
+// es/usantatecla/aX_menu/a9_lambda funciones en opciones! No en main!!!
 
 // default
 // static
